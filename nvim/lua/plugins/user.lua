b2638fa821285846160b769653b665f5fdd1a516
Jon Perry <yrrepnoj@gmail.com>
2025-09-09 12:51:48 -0400

    i really dont know what i was doing here..

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -13,7 +13,42 @@ return {
     config = function() require("lsp_signature").setup() end,
   },
 
-  -- == Examples of Overriding Plugins ==
+  -- Override the configs for neo-tree os that I can add files to Avante easier..
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    config = function()
+      require("neo-tree").setup {
+        filesystem = {
+          commands = {
+            avante_add_files = function(state)
+              local node = state.tree:get_node()
+              local filepath = node:get_id()
+              local relative_path = require("avante.utils").relative_path(filepath)
+
+              local sidebar = require("avante").get()
+
+              local open = sidebar:is_open()
+              -- ensure avante sidebar is open
+              if not open then
+                require("avante.api").ask()
+                sidebar = require("avante").get()
+              end
+
+              sidebar.file_selector:add_selected_file(relative_path)
+
+              -- remove neo tree buffer
+              if not open then sidebar.file_selector:remove_selected_file "neo-tree filesystem [1]" end
+            end,
+          },
+          window = {
+            mappings = {
+              ["oa"] = "avante_add_files",
+            },
+          },
+        },
+      }
+    end,
+  },
 
   -- customize alpha options
   {
