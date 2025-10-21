fe367343fe0acc151da4b9252777f597038cb7d5
Jon Perry <yrrepnoj@gmail.com>
2025-10-20 23:05:47 -0400

    remove legendary plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,31 +31,6 @@ vim.api.nvim_create_autocmd('VimEnter', {
 require "lazy_setup"
 require "polish"
 
-require('legendary').setup({
-  extensions = {
-    lazy_nvim = true,
-
-    which_key = {
-      -- Automatically add which-key tables to legendary
-      -- see WHICH_KEY.md for more details
-      auto_register = false,
-      -- you can put which-key.nvim tables here,
-      -- or alternatively have them auto-register,
-      -- see WHICH_KEY.md
-      mappings = {},
-      opts = {},
-      -- controls whether legendary.nvim actually binds they keymaps,
-      -- or if you want to let which-key.nvim handle the bindings.
-      -- if not passed, true by default
-      do_binding = true,
-      -- controls whether to use legendary.nvim item groups
-      -- matching your which-key.nvim groups; if false, all keymaps
-      -- are added at toplevel instead of in a group.
-      use_groups = true,
-    },
-  },
-})
-
 -- Move window borders
 vim.keymap.set('n', '<C-M-h>', ':vertical resize +5<CR>', { noremap = true, silent = true }) -- Move border left
 vim.keymap.set('n', '<C-M-l>', ':vertical resize -5<CR>', { noremap = true, silent = true }) -- Move border right
