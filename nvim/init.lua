26e4cf6c661db8482dff69ad93d9d4e93a9b55e8
Jon Perry <yrrepnoj@gmail.com>
2026-01-21 10:23:52 -0500

    nvim: fix venv-selector plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,19 +15,6 @@ if not pcall(require, "lazy") then
   vim.cmd.quit()
 end
 
-vim.api.nvim_create_autocmd('VimEnter', {
-  desc = 'Auto select virtualenv Nvim open',
-  pattern = '*',
-  callback = function()
-    local venv = vim.fn.findfile('pyproject.toml', vim.fn.getcwd() .. ';')
-    if venv ~= '' then
-      require('venv-selector').retrieve_from_cache()
-    end
-  end,
-  once = true,
-})
-
-
 require "lazy_setup"
 require "polish"
 
--- nvim/lua/plugins/venv-selector.lua
+++ nvim/lua/plugins/venv-selector.lua
@@ -1,19 +1,15 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 return {
-	"linux-cultist/venv-selector.nvim",
-	dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-	opts = {
-		-- Your options go here
-		name = ".venv",
-		auto_refresh = true,
-	},
-	event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-	keys = {
-		-- Keymap to open VenvSelector to pick a venv.
-		{ "<leader>vs", "<cmd>VenvSelect<cr>" },
-		-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-		{ "<leader>vc", "<cmd>VenvSelectCached<cr>" },
-	},
+  "linux-cultist/venv-selector.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
+  },
+  ft = "python", -- Load when opening Python files
+  keys = {
+    { ",v", "<cmd>VenvSelect<cr>" }, -- Open picker on keymap
+  },
+  opts = { -- this can be an empty lua table - just showing below for clarity.
+    search = {}, -- if you add your own searches, they go here.
+    options = {}, -- if you add plugin options, they go here.
+  },
 }
-
