14f0f518ed32443bd9ed86a0508a0ba3d4dc7b3a
Jon Perry <yrrepnoj@gmail.com>
2026-01-21 12:23:52 -0500

    nvim: add claudecode and copilot configs

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,25 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  event = "BufReadPost",
+  opts = {
+    suggestion = {
+      enabled = not vim.g.ai_cmp,
+      auto_trigger = true,
+      hide_during_completion = vim.g.ai_cmp,
+      keymap = {
+        accept = "<C-;>",
+        accept_word = "<C-h>",
+        accept_line = "<C-l>",
+        next = "<M-]>",
+        prev = "<M-[>",
+      },
+    },
+    panel = { enabled = false },
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
+  },
+}
