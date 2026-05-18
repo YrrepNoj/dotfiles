f2bd819a193398014635c0e5d44976d132ddd52e
Jon Perry <yrrepnoj@gmail.com>
2026-05-18 14:42:08 -0400

    chore(nvim): disable copilot by default

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,25 +1,27 @@
-return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  event = "BufReadPost",
-  opts = {
-    suggestion = {
-      enabled = not vim.g.ai_cmp,
-      auto_trigger = true,
-      hide_during_completion = vim.g.ai_cmp,
-      keymap = {
-        accept = "<C-;>",
-        accept_word = "<C-h>",
-        accept_line = "<C-l>",
-        next = "<M-]>",
-        prev = "<M-[>",
-      },
-    },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
-    },
-  },
-}
+return {}
+
+-- return {
+--   "zbirenbaum/copilot.lua",
+--   cmd = "Copilot",
+--   build = ":Copilot auth",
+--   event = "BufReadPost",
+--   opts = {
+--     suggestion = {
+--       enabled = not vim.g.ai_cmp,
+--       auto_trigger = true,
+--       hide_during_completion = vim.g.ai_cmp,
+--       keymap = {
+--         accept = "<C-;>",
+--         accept_word = "<C-h>",
+--         accept_line = "<C-l>",
+--         next = "<M-]>",
+--         prev = "<M-[>",
+--       },
+--     },
+--     panel = { enabled = false },
+--     filetypes = {
+--       markdown = true,
+--       help = true,
+--     },
+--   },
+-- }
